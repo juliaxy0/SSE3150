566c74532bab7def4e893f5e42027ad8227f56a2
Julia <89339830+juliaxy0@users.noreply.github.com>
2022-04-20 10:29:10 +0800

    Update index.php

--- LAB3/Q1/index.php
+++ LAB3/Q1/index.php
@@ -1,1 +1,34 @@
 
+<html lang="en">
+ <head>
+ <title>Julia Nurfadhilah 208256</title>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+
+ </head>
+
+ <body>
+
+    <h1>Julia Nurfadhilah PHP</h1>
+    
+
+    ASCII ART:
+    <pre>
+      *****
+           *
+           *
+           *
+      *    *
+      **
+    </pre>
+
+    <?php
+    echo (" The SHA256 hash of Julia Nurfadhilah is: <br><br> ");
+    echo hash('sha256', 'Julia Nurfadhilah');
+  ?>
+<br><br>
+  <a href="check.php">Click here to check the error setting</a><br>
+  <a href="fail.php">Click here to cause a traceback</a><br>
+ </body>
+
+</html>
